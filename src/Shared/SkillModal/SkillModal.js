7ccec3fe77f79a322d93fe7bf21afa022d689daf
Nayon <nayonroy64@gmail.com>
2022-12-18 20:39:11 +0600

    add position of skill modal

--- src/Shared/SkillModal/SkillModal.js
+++ src/Shared/SkillModal/SkillModal.js
@@ -3,13 +3,17 @@ import React from 'react';
 const SkillModal = ({ props, setProps }) => {
 	console.log(props)
 	return (
-    <div className="absolute card w-96 bg-base-100 shadow-xl">
-      <div className="card-body">
-        <h2 className="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
-          <button className="btn btn-primary" onClick={()=>setProps(null)}>Cancle</button>
+    <div className="fixed top-0 flex items-center w-screen justify-center  h-full">
+      <div className=" card w-96 bg-red-100 shadow-xl mx-auto">
+        <div className="card-body">
+          <h2 className="card-title">Card title!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+            <button className="btn btn-primary" onClick={() => setProps(null)}>
+              Cancle
+            </button>
+          </div>
         </div>
       </div>
     </div>
